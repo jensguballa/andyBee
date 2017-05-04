af6541e6a86fc8835c7d5a5968f24f287a5ffc14
Jens Guballa <jens@guballa.de>
2017-05-04 22:54:26 +0200

    Started to design filter dialog

--- app/static/main/andyBee.module.js
+++ app/static/main/andyBee.module.js
@@ -1,6 +1,6 @@
 (function () {
     angular
-        .module('andyBeeApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ngResource', 'ngTable', 'leaflet-directive'])
+        .module('andyBeeApp', ['ngSanitize', 'ui.bootstrap', 'ngResource', 'ngTable', 'leaflet-directive'])
         .config(function($logProvider) {
             $logProvider.debugEnabled(false);
         })
