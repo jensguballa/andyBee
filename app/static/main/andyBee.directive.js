d509ae3398d8a3bf3d1faae3064a5b0fb979f31b
Jens Guballa <jens@guballa.de>
2017-05-15 23:05:28 +0200

    More work on detailed geocache section
    - Main menu: remove rounded corners
    - Add icons for rating and cache size
    - HTML changes

--- app/static/main/andyBee.directive.js
+++ app/static/main/andyBee.directive.js
@@ -5,7 +5,9 @@
         .module('andyBeeApp')
         .directive('convertToNumber', convertToNumber)
         .directive('fileModel', fileModel)
-        .directive('geocacheIcon', geocacheIcon);
+        .directive('geocacheIcon', geocacheIcon)
+        .directive('geocacheRating', geocacheRating)
+        .directive('geocacheSize', geocacheSize);
             
     function convertToNumber() {
         return {
@@ -37,36 +39,96 @@
         };
     }
 
-    var img_trans = {
+    var type_trans = {
         'Traditional Cache':        'static/images/OCMLogoSmall.svg',
         'Letterbox Hybrid':         'static/images/letterbox.svg',
-        'Event Cache':              'static/images/event.svg',                  
-        'Multi-cache':              'static/images/multi.svg',                  
-        'Wherigo Cache':            'static/images/wherigo.svg',                  
-        'Mega-Event Cache':         'static/images/mega.svg',                  
-        'Unknown Cache':            'static/images/unknown.svg',                  
-        'Earthcache':               'static/images/earth.svg',                  
-        'Cache In Trash Out Event': 'static/images/cito.svg',                  
-        'Virtual Cache':            'static/images/virtual.svg',                  
-        'GPS Adventures Exhibit':   'static/images/other.svg',                  
-        'Webcam Cache':             'static/images/webcam.svg',                  
-        'Project APE Cache':        'static/images/other.svg',                  
-        'Locationless Cache':       'static/images/other.svg',                  
+        'Event Cache':              'static/images/event.svg',
+        'Multi-cache':              'static/images/multi.svg',
+        'Wherigo Cache':            'static/images/wherigo.svg',
+        'Mega-Event Cache':         'static/images/mega.svg',
+        'Unknown Cache':            'static/images/unknown.svg',
+        'Earthcache':               'static/images/earth.svg',
+        'Cache In Trash Out Event': 'static/images/cito.svg',
+        'Virtual Cache':            'static/images/virtual.svg',
+        'GPS Adventures Exhibit':   'static/images/other.svg',
+        'Webcam Cache':             'static/images/webcam.svg',
+        'Project APE Cache':        'static/images/other.svg',
+        'Locationless Cache':       'static/images/other.svg',
     };
 
     function geocacheIcon() {
         return {
             restrict: 'E',
-            template: '<img ng-src="{{src}}" class="{{class}}" height="{{height}}" />',
+            template: '<img ng-src="{{src}}" class="{{class}}" />',
             link: link,
+            scope: {}
         };
 
         function link(scope, elem, attr) {
             scope.class = attr.class;
-            scope.height = attr.height || "20px";
 
             attr.$observe('type', function () {
-                scope.src = img_trans[attr.type];
+                scope.src = type_trans[attr.type];
+            });
+        }
+    }
+
+    var size_trans = {
+        'Micro':      'static/images/size_micro.svg',
+        'Small':      'static/images/size_small.svg',
+        'Regular':    'static/images/size_regular.svg',
+        'Large':      'static/images/size_large.svg',
+        'Other':      'static/images/other.svg',
+        'Virtual':    'static/images/other.svg',
+        'Not chosen': 'static/images/other.svg'
+    };
+
+    function geocacheSize() {
+        return {
+            restrict: 'E',
+            template: '<img ng-src="{{src}}" class="{{class}}" />',
+            link: link,
+            scope: {}
+        };
+
+        function link(scope, elem, attr) {
+            scope.class = attr.class;
+
+            attr.$observe('size', function () {
+                scope.src = size_trans[attr.size];
+            });
+        }
+    }
+    function geocacheRating() {
+        return {
+            restrict: 'E',
+            template: '<img ng-src="{{src[0]}}" class="{{class}}" />' +
+                      '<img ng-src="{{src[1]}}" class="{{class}}" />' +
+                      '<img ng-src="{{src[2]}}" class="{{class}}" />' +
+                      '<img ng-src="{{src[3]}}" class="{{class}}" />' +
+                      '<img ng-src="{{src[4]}}" class="{{class}}" />',
+            link: link,
+            scope: {}
+        };
+
+        function link(scope, elem, attr) {
+            scope.class = attr.class;
+
+            attr.$observe('value', function () {
+                scope.src = [];
+                var rating = attr.value;
+                for (var i = 0; i < 5; i++) {
+                    if (rating >= 1) {
+                        scope.src[i] = 'static/images/star.svg';
+                    }
+                    else if (rating > 0) {
+                        scope.src[i] = 'static/images/halfstar.svg';
+                    }
+                    else {
+                        scope.src[i] = 'static/images/star_empty.svg';
+                    }
+                    rating--;
+                }
             });
         }
     }
