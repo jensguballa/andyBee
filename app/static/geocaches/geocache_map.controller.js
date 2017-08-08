d56e548de455339cfb8bcabf40d1e4f32fd08283
Jens Guballa <jens@guballa.de>
2017-08-08 20:05:11 +0200

    Map center
    - Option on the map added to show the current map center
    (blue cirle)
    - Button added on the map to zoom to the current map center
    - Selecting a geocache as a map center will zoom into it on
    the map

--- app/static/geocaches/geocache_map.controller.js
+++ app/static/geocaches/geocache_map.controller.js
@@ -8,8 +8,42 @@
     GeocacheMapCtrl.$inject = ['$scope', '$timeout', 'GeocacheService'];
     function GeocacheMapCtrl($scope, $timeout, GeocacheService) {
 
+        var marker_trans = {
+            'Traditional Cache':        'static/images/marker/traditional.svg',
+            'Letterbox Hybrid':         'static/images/marker/letterbox.svg',
+            'Event Cache':              'static/images/marker/event.svg',
+            'Multi-cache':              'static/images/marker/multi.svg',
+            'Wherigo Cache':            'static/images/marker/wherigo.svg',
+            'Mega-Event Cache':         'static/images/marker/mega.svg',
+            'Unknown Cache':            'static/images/marker/unknown.svg',
+            'Earthcache':               'static/images/marker/earth.svg',
+            'Cache In Trash Out Event': 'static/images/marker/cito.svg',
+            'Virtual Cache':            'static/images/marker/virtual.svg',
+            'GPS Adventures Exhibit':   'static/images/marker/adventures.svg',
+            'Webcam Cache':             'static/images/marker/webcam.svg',
+            'Project APE Cache':        'static/images/marker/ape.svg',
+            'Locationless Cache':       'static/images/marker/locationless.svg',
+            'Giga-Event Cache':         'static/images/marker/giga.svg',
+        };
+
         var vm = this;
+        vm.centroid = {
+            lat: 0,
+            lng: 0,
+            zoom: 1
+        };
+        var centroid_marker = {
+            latlngs: [vm.centroid.lat, vm.centroid.lng],
+            color: '#0000ff',
+            radius: 30,
+            weight: 1,
+            opacity: 1,
+            type: 'circleMarker',
+            layer: 'centroid'
+        }
         vm.markers = [];
+        vm.circles = [];
+        vm.circles_hidden = [];
         vm.layers = {
             baselayers: {
                 osm: {
@@ -30,21 +64,74 @@
                         disableClusteringAtZoom: 14,
                         spiderfyOnMaxZoom: false
                     }
+                },
+                centroid: {
+                    name: "Show Map Center",
+                    type: "group",
+                    visible: true
+                },
+                circles: {
+                    name: "Minimum Geocache Distance",
+                    type: "group",
+                    visible: true
                 }
             }
         };
+
+        vm.center_map = center_map;
+
         $scope.$on('geocaches_updated', function (event, args) {
             vm.markers = [];
+            vm.circles = [centroid_marker];
+            vm.circles_hidden = [];
             for (var i = 0, len = GeocacheService.geocache_list.length; i < len; i++) {
                 var geocache = GeocacheService.geocache_list[i];
                 vm.markers.push({
                     lat: geocache.lat,
                     lng: geocache.lon,
                     message: geocache.title,
-                    layer: "geocaches"
+                    layer: "geocaches",
+                    icon: {
+                        iconUrl: marker_trans[geocache.type],
+                        iconSize:     [36, 36], // size of the icon
+                        iconAnchor:   [18, 36], // point of the icon which will correspond to marker's location
+//                        shadowUrl:    'static/images/marker/shadow.svg',
+//                        shadowSize:   [20, 20], // size of the shadow
+//                        shadowAnchor: [10, 10],  // the same for the shadow
+                        popupAnchor:  [0, -36] // point from which the popup should open relative to the iconAnchor
+                    }
                 });
+//                vm.circles_hidden.push({
+//                    latlngs: [geocache.lat, geocache.lon],
+//                    color: '#ff0000',
+//                    radius: 161,
+//                    weight: 1,
+//                    opacity: 1,
+//                    type: 'circle',
+//                    layer: 'circles'
+//                });
             }
         });
+
+        $scope.$on('center_updated', function (event, args) {
+            center_map()
+        });
+
+//        $scope.$watch("map.centroid.zoom", function (zoom) {
+//            if (zoom >= 14) {
+//                vm.circles = vm.circles_hidden;
+//            }
+//            else {
+//                vm.circles = [];
+//            }
+//        });
+
+        function center_map() {
+            vm.centroid.lat = GeocacheService.home_lat;
+            vm.centroid.lng = GeocacheService.home_lon;
+            vm.centroid.zoom = 14;
+            centroid_marker.latlngs = [GeocacheService.home_lat, GeocacheService.home_lon];
+        }
 
     }
 
